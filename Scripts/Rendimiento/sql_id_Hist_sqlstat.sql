61ea0b2d3ed17b38318529bc3008b59ba6529076
JAG <jagranadosr@gmail.com>
2016-03-19 23:37:13 +0100

    Rendimiento

---
+++ Scripts/Rendimiento/sql_id_Hist_sqlstat.sql
@@ -0,0 +1,27 @@
+-- The query mines the AWR tables (beware the licence implications) for a specific SQL ID 
+-- and date/time range and shows a few choice statistics for each snapshot period
+-- https://oraganism.wordpress.com/2011/12/14/a-dba_hist_sqlstat-query-that-i-am-very-fond-of/
+
+prompt enter start and end times in format DD-MON-YYYY [HH24:MI]
+  
+column sample_end format a21
+select to_char(min(s.end_interval_time),'DD-MON-YYYY DY HH24:MI') sample_end
+, q.sql_id
+, q.plan_hash_value
+, sum(q.EXECUTIONS_DELTA) executions
+, round(sum(DISK_READS_delta)/greatest(sum(executions_delta),1),1) pio_per_exec
+, round(sum(BUFFER_GETS_delta)/greatest(sum(executions_delta),1),1) lio_per_exec
+, round((sum(ELAPSED_TIME_delta)/greatest(sum(executions_delta),1)/1000),1) msec_exec
+from dba_hist_sqlstat q, dba_hist_snapshot s
+where q.SQL_ID=trim('&sqlid.')
+and s.snap_id = q.snap_id
+and s.dbid = q.dbid
+and s.instance_number = q.instance_number
+and s.end_interval_time >= to_date(trim('&start_time.'),'dd-mon-yyyy hh24:mi')
+and s.begin_interval_time <= to_date(trim('&end_time.'),'dd-mon-yyyy hh24:mi')
+and substr(to_char(s.end_interval_time,'DD-MON-YYYY DY HH24:MI'),13,2) like '%&hr24_filter.%'
+group by s.snap_id
+, q.sql_id
+, q.plan_hash_value
+order by s.snap_id, q.sql_id, q.plan_hash_value
+/
